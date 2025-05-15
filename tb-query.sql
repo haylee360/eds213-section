68a8c7232d676106a7f1faf40dd829d19f8b9e21
haylee360 <hayleeoyler360@gmail.com>
2025-05-14 20:34:35 -0700

    Clean up viz and sql

--- tb-query.sql
+++ tb-query.sql
@@ -42,13 +42,6 @@ SELECT * FROM socioecon;
 
 
 -- Question: How is tuberculosis incidence correlated with GDP?
-
-SELECT MAX(Incidence) AS Max_incidence, CountryCode, Year FROM tuberculosis
-    JOIN socioecon USING (CountryCode, Year);
-    GROUP BY (CountryCode, Year)
-    ORDER BY Max_incidence DESC
-    LIMIT 15;
-
 CREATE TABLE tb_gdp AS
     SELECT tb.CountryCode, c.CountryName, tb.Year, tb.Incidence, s.GDPCapita
     FROM tuberculosis tb
@@ -58,61 +51,20 @@ CREATE TABLE tb_gdp AS
 
 SELECT * FROM tb_gdp;
 
-
-SELECT tb.CountryCode, c.CountryName, tb.Year, 
-    MAX(tb.Incidence) AS Max_incidence, ANY_VALUE(s.GDPCapita) AS GDPCapita
+-- Countries with the highest TB rates
+SELECT tb.CountryCode, c.CountryName, tb.Year, tb.Incidence, s.GDPCapita
     FROM tuberculosis tb
     JOIN country c ON tb.CountryCode = c.CountryCode
     JOIN socioecon s ON tb.CountryCode = s.CountryCode AND tb.Year = s.Year
-    WHERE tb.Incidence IS NOT NULL AND s.GDPCapita IS NOT NULL 
-        AND tb.Year = 2023
-    GROUP BY tb.CountryCode, c.CountryName, tb.Year
-    ORDER BY Max_incidence DESC
+    WHERE tb.Incidence IS NOT NULL AND s.GDPCapita IS NOT NULL AND tb.Year = 2023
+    ORDER BY Incidence DESC
     LIMIT 10;
 
-SELECT tb.CountryCode, c.CountryName, tb.Year, 
-    ANY_VALUE(tb.Incidence) AS Incidence, MAX(s.GDPCapita) AS Max_GDPCapita
+-- Richest countries
+SELECT tb.CountryCode, c.CountryName, tb.Year, tb.Incidence, s.GDPCapita
     FROM tuberculosis tb
     JOIN country c ON tb.CountryCode = c.CountryCode
     JOIN socioecon s ON tb.CountryCode = s.CountryCode AND tb.Year = s.Year
-    WHERE tb.Incidence IS NOT NULL AND s.GDPCapita IS NOT NULL 
-        AND tb.Year = 2023
-    GROUP BY tb.CountryCode, c.CountryName, tb.Year
-    ORDER BY Max_GDPCapita DESC
-    LIMIT 10;
-
-SELECT tb.CountryCode, c.CountryName, tb.Year, 
-    MAX(tb.Incidence) AS Incidence, AVG(s.GDPCapita) AS GDPCapita
-    FROM tuberculosis tb
-    JOIN country c ON tb.CountryCode = c.CountryCode
-    JOIN socioecon s ON tb.CountryCode = s.CountryCode AND tb.Year = s.Year
-    WHERE tb.Incidence IS NOT NULL AND s.GDPCapita IS NOT NULL 
-        AND tb.Year = 2023
-    GROUP BY tb.CountryCode, c.CountryName, tb.Year
+    WHERE tb.Incidence IS NOT NULL AND s.GDPCapita IS NOT NULL AND tb.Year = 2023
     ORDER BY GDPCapita DESC
     LIMIT 10;
-
-
-SELECT * FROM a LEFT JOIN b ON a.common = b.common;
-
-SELECT * FROM socioecon 
-    WHERE CountryCode = 'MCO';
-
-SELECT * FROM tuberculosis 
-    WHERE CountryCode = 'MCO';
-
-SELECT * FROM country 
-    WHERE CountryCode = 'MCO';
-
-SELECT * FROM tb_gdp
-    WHERE CountryCode = 'MCO';
-
-CREATE TABLE country_deduped AS
-SELECT DISTINCT CountryCode, CountryName, ContinentCode, ContinentName
-FROM country;
-
-SELECT * FROM country_deduped
-    WHERE CountryCode = 'MCO';
-
-DROP TABLE country;
-ALTER TABLE country_deduped RENAME TO country;
